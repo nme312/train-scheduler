d9df267a1f63b80316d2649da5a2b28e8f4bc214
SkyNet <nme312.noreply.github.com>
2018-05-02 10:04:19 -0500

    now submiting name dest. and freq. to table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,87 @@
-//create variables to store train info
-var trainName = [];
-var destination = [];
-var frequency = [];
-var nextArrival = [];
-var minutesAway = [];
+$(document).ready(function () {
 
-//create jquery objects for user input
-var trainNameInput = $("#name");
-var destinationInput = $("#destination");
-var firstTimeInput = $("#first-time");
-var frequencyInput = $("#frequency");
+    //create arrays to store train info
+    var trainName = [];
+    var destination = [];
+    var frequency = [];
+    var nextArrival = [];
+    var minutesAway = [];
 
-//stringify and store train info in localStorage
-JSON.stringify(trainName);
-JSON.stringify(destination);
-JSON.stringify(frequency);
-JSON.stringify(nextArrival);
-JSON.stringify(minutesAway);
+    //create jquery objects for user input
+    var trainNameInput = $("#name");
+    var destinationInput = $("#destination");
+    var firstTimeInput = $("#first-time");
+    var frequencyInput = $("#frequency");
 
-//link localStorage to html elements
-localStorage.setItem("trainName", trainName);
-localStorage.setItem("destination", destination);
-localStorage.setItem("frequency", frequency);
-localStorage.setItem("nextArrival", nextArrival);
-localStorage.setItem("minutesAway", minutesAway);
+    //stringify and store train info in localStorage
+    JSON.stringify(trainName);
+    JSON.stringify(destination);
+    JSON.stringify(frequency);
+    JSON.stringify(nextArrival);
+    JSON.stringify(minutesAway);
 
-//create on.click function to add current values of user input
-//to local storage and calculate next-arrival and minutes-away
-var submitBtn = $("#submit-button");
-var trainTable = $("<td>");
-var destinationTable = $("<td>");
-var frequencyTable = $("<td>");
+    //create on.click function to add current values of user input
+    //to local storage and calculate next-arrival and minutes-away
+    var submitBtn = $("#submit-button");
+    var tableBody = $("#table-body")
 
-trainTable.val("");
-submitBtn.on("click", function () {
-    console.log(trainNameInput.val());
-    console.log(destinationInput.val());
-    console.log(frequencyInput.val());
+    submitBtn.on("click", function () {
+        //creating jq objects
+        var tableRow = $("<tr>");
+        var trainNameTable = $("<td>");
+        var destinationTable = $("<td>");
+        var frequencyTable = $("<td>");
 
-    trainTable.val(trainNameInput);
-    destinationTable.text(destinationInput);
-    frequencyTable.val(frequencyInput);
+        console.log(trainNameInput.val());
+        console.log(destinationInput.val());
+        console.log(moment(frequencyInput.val(), "h:mm a").format("h:mm a"));
 
-    $("#table-contents").append(trainTable);
-    $("#table-contents").append(destinationTable);
-    $("#table-contents").append(frequencyTable);
+        //converting user input
+        var foramtedFrequency = moment(frequencyInput.val(), "h:mm a").format("h:mm a");
+        var convertedHours = moment(foramtedFrequency).hours() * 60;
+        console.log(convertedHours + " hours > min.")
+        var convertedFrequency;
+        console.log(foramtedFrequency + " formated");
+        console.log(convertedFrequency + " converted");
 
-    //use user inputs to calculate nextArrival and minutesAway
+        //adding user input to table objects
+        trainNameTable.text(trainNameInput.val());
+        destinationTable.text(destinationInput.val());
+        frequencyTable.text(convertedFrequency);
 
-    //write for loop to push input into localStorage variables
+        //adding user input to localStorage array
+        trainName.push(trainNameInput.val());
+        destination.push(destinationInput.val());
+        frequency.push(convertedFrequency);
 
-    //write for loop to display current localStorage values in train table
+        //store stagnate train info in localStorage
+        localStorage.setItem("trainNames", trainName);
+        localStorage.setItem("destinations", destination);
+        localStorage.setItem("frequencies", frequency);
 
-    //add if/else that allows user to edit existing trains using train name
+        //creating variables for localStorage items
+        var localTrainNames = localStorage.getItem("trainNames");
+        var localDestinations = localStorage.getItem("destinations");
+        var localFrequencies = localStorage.getItem("frequencies");
 
-    //add if/else that prevents user from submiting form without filling out all fields
+        //appending table objects to table row object
+        tableRow.append(trainNameTable);
+        tableRow.append(destinationTable);
+        tableRow.append(frequencyTable);
+
+        //appending table row object to table body object
+        tableBody.append(tableRow);
+
+
+        //use user inputs to calculate nextArrival and minutesAway
+
+        //write for loop to push input into localStorage variables
+
+        //write for loop to display current localStorage values in train table
+
+        //add if/else that allows user to edit existing trains using train name
+
+        //add if/else that prevents user from submiting form without filling out all fields
+    })
+
 });
